6091699797eb68ed1425f6d0e3028cdb5610408a
Areckyfox <arkadiusz.lis@vp.pl>
2019-02-23 22:04:34 +0100

    on respond send file html, rootDir in use in path

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const rootDir = require("./util/path");
+
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,12 +12,12 @@ const shopRouters = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use(adminRoutes);
 
 app.use(shopRouters);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "error404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,13 @@
+const rootDir = require("../util/path");
+
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<form action="/admin/product" method="POST">
-      <input type="text" name="product"> 
-      <button type="submit">submit</button>
-    </form>`
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 router.post("/product", (req, res, next) => {
   console.log(req.body);
